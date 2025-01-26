c3f711151e481996f90b9b61602000932456b941
Samuel Edmondson <sedmondson@wisc.edu>
2025-01-26 14:47:42 -0600

    make fee text red and visible

--- scenes/fee_cost.gd
+++ scenes/fee_cost.gd
@@ -2,7 +2,7 @@ extends Label
 
 func update_fee(value):
 	text = "Fee: $" + str(round(value))
-	set("theme_override_color/font_color", Color.RED)
+	set("theme_override_colors/font_color", Color.RED)
 
 func clear_fee_cost():
 	text = ""
--- scenes/fee_timer_left.gd
+++ scenes/fee_timer_left.gd
@@ -2,7 +2,7 @@ extends Label
 
 func update_fee_timer(value):
 	text = str(round(value)) + "s"
-	set("theme_override_color/font_color", Color.RED)
+	set("theme_override_colors/font_color", Color.RED)
 
 func clear_fee_timer():
 	text = ""
